d93cc415d6effd9e065f254d473eaafd32cb7bb6
harmlessprince <realolamilekan@gmail.com>
2023-09-12 20:20:05 +0100

    addded production env

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -3,10 +3,20 @@ package initializers
 import (
 	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
+
+	env := os.Getenv("APP_ENV")
+	if "" == env {
+		env = "development"
+	}
+	err := godotenv.Load(".env." + env)
+
+	if "test" == env {
+		err = godotenv.Load(".env.test")
+	}
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
