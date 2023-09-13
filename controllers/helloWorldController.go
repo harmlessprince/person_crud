9bd3c0fa3ff956013c0e1790cf9ae8f052db7221
harmlessprince <realolamilekan@gmail.com>
2023-09-13 12:26:45 +0100

    updated home url

--- controllers/helloWorldController.go
+++ controllers/helloWorldController.go
@@ -18,5 +18,12 @@ import (
 // @Router /api [get]
 
 func HelloWorld(g *gin.Context) {
-	g.JSON(http.StatusOK, "hello world")
+	g.JSON(http.StatusOK, gin.H{
+		"success":               true,
+		"message":               "We are are alive ):",
+		"base_url":              "https://taofeeq-hng-stage-two.onrender.com/",
+		"github_repo":           "https://github.com/harmlessprince/person_crud",
+		"github_documentation":  "https://github.com/harmlessprince/person_crud#readme",
+		"swagger_documentation": "https://taofeeq-hng-stage-two.onrender.com/docs/index.html",
+	})
 }
